179c3bb7791db8f3f6837e0d0c91f5a04d05e462
ShinJaehun <gunsmoke.shin@gmail.com>
2022-11-05 15:52:56 +0900

    parallelization

--- main.js
+++ main.js
@@ -12,38 +12,80 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 // const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
+// const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
+const N = 100;
+const cars = generateCars(N);
+let bestCar = cars[0];
 
+if (localStorage.getItem("bestBrain")) {
+    bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
+}
 
 // car.draw(ctx);  // 이거 그대로 둬서 sensor update할 때 계속 알 수 없는 오류가 있었음.
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2)
 ];
 
 animate();
+
+function save() {
+    localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+    localStorage.removeItem("bestBrain");
+}
+
+function generateCars(N) {
+    const cars = [];
+    for (let i = 1; i <= N; i++) {
+        cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"))
+    }
+    return cars;
+}
+
 
 function animate(time) {
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].update(road.borders, []); // traffic끼리 collision 처리하지 말고
     }
 
-    car.update(road.borders, traffic); // car와 traffic의 collision 처리만!
+    // car.update(road.borders, traffic); // car와 traffic의 collision 처리만!
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].update(road.borders, traffic);
+    }
+
+    bestCar = cars.find(
+        c => c.y == Math.min(...cars.map(c=>c.y))
+    );
 
     carCanvas.height = window.innerHeight;
     networkCanvas.height = window.innerHeight;
 
     carCtx.save();
-    carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+    // carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+    carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
 
     road.draw(carCtx);
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].draw(carCtx, "red");
     }
-    car.draw(carCtx, "blue");
+
+    carCtx.globalAlpha = 0.2;
+    
+    // car.draw(carCtx, "blue");
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].draw(carCtx, "blue");
+    }
+
+    carCtx.globalAlpha = 1;
+    bestCar.draw(carCtx, "blue", true);
 
     carCtx.restore();
 
     networkCtx.lineDashOffset = -time / 50;
-    Visualizer.drawNetwork(networkCtx, car.brain);
+    Visualizer.drawNetwork(networkCtx, bestCar.brain);
     requestAnimationFrame(animate);
 }
