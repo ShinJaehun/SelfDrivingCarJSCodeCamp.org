7b5e2acd49f5484ddffedea71bc17b73b8f088cc
ShinJaehun <gunsmoke.shin@gmail.com>
2022-11-05 15:21:55 +0900

    neural network

--- car.js
+++ car.js
@@ -12,8 +12,13 @@ class Car {
         this.angle = 0;
         this.damaged = false;
 
+        this.useBrain = controlType == "AI";
+
         if (controlType != "DUMMY") {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork(
+                [this.sensor.rayCount, 6, 4]
+            );
         }
         this.controls = new Controls(controlType);
     }
@@ -26,7 +31,19 @@ class Car {
         }
         if (this.sensor) {
             this.sensor.update(roadBoarders, traffic); // traffic의 sensor
-        }
+            const offsets = this.sensor.readings.map(
+                s => s == null? 0 : 1-s.offset // 이해 필요
+            );
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+            // console.log(outputs);
+
+            if (this.useBrain) {
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
+        }   
     }
 
     #assessDamage(roadBoarders, traffic) {
--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-const canvas = document.getElementById("myCanvas");
+const carCanvas = document.getElementById("carCanvas");
 // canvas.height = window.innerHeight;
-canvas.width = 200;
+carCanvas.width = 200;
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const networkCanvas = document.getElementById("networkCanvas");
+// canvas.height = window.innerHeight;
+networkCanvas.width = 300;
+
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
+// const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
+
 
 // car.draw(ctx);  // 이거 그대로 둬서 sensor update할 때 계속 알 수 없는 오류가 있었음.
 const traffic = [
@@ -13,24 +22,28 @@ const traffic = [
 
 animate();
 
-function animate() {
+function animate(time) {
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].update(road.borders, []); // traffic끼리 collision 처리하지 말고
     }
 
     car.update(road.borders, traffic); // car와 traffic의 collision 처리만!
 
-    canvas.height = window.innerHeight;
+    carCanvas.height = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
 
-    ctx.save();
-    ctx.translate(0, -car.y + canvas.height * 0.7);
+    carCtx.save();
+    carCtx.translate(0, -car.y + carCanvas.height * 0.7);
 
-    road.draw(ctx);
+    road.draw(carCtx);
     for (let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(ctx, "red");
+        traffic[i].draw(carCtx, "red");
     }
-    car.draw(ctx, "blue");
+    car.draw(carCtx, "blue");
 
-    ctx.restore();
+    carCtx.restore();
+
+    networkCtx.lineDashOffset = -time / 50;
+    Visualizer.drawNetwork(networkCtx, car.brain);
     requestAnimationFrame(animate);
 }
